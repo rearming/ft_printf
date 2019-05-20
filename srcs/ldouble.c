d6323370220298544738cbc8787f3e5ab2c0fde5
Sole Selusa <sselusa@oa-l3.21-school.ru>
2019-05-20 18:03:17 +0300

    Add ldouble

--- srcs/ldouble.c
+++ srcs/ldouble.c
@@ -6,7 +6,7 @@
 /*   By: sselusa <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/15 12:26:00 by sselusa           #+#    #+#             */
-/*   Updated: 2019/05/20 17:42:29 by sselusa          ###   ########.fr       */
+/*   Updated: 2019/05/20 18:03:06 by sselusa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,30 @@
 #include <stdio.h>
 #include <limits.h>
 
+static void				get_a(t_ld *ld, int sign,
+								long double num, int precision)
+{
+	unsigned long		whole;
+	unsigned long		fract;
+
+	whole = (unsigned long)num;
+	fract = (unsigned long)(ft_power(10, precision) * (num - whole));
+	if (sign == -1)
+		ld->str = ft_strdup("-");
+	else
+		ld->str = ft_strnew(0);
+	ld->str = ft_strcat(ld->str, ft_ultoa(whole));
+	ld->str = ft_strcat(ld->str, ".");
+	ld->str = ft_strncat(ld->str, ft_ultoa(fract), (size_t)precision);
+}
+
 static t_ld				*parse_ld(long double num, int precision)
 {
 	__int128_t			mask;
-	long				whole;
-	long				fract;
+	int					sign;
 	t_ld				*ld;
 
+	num = (double)num;
 	if (!(ld = (t_ld*)malloc(sizeof(t_ld))))
 		return (NULL);
 	ld->str = NULL;
@@ -32,21 +49,26 @@ static t_ld				*parse_ld(long double num, int precision)
 	ld->negative = (int)((ld->ld.int_tmp & mask) >> 73);
 	if (check_fields(ld))
 		return (ld);
-	whole = (long)num;
-	fract = (ft_power(10, precision) * (long)num) - whole;
+	sign = 1;
+	if (num < 0)
+	{
+		sign = -1;
+		num *= -1;
+	}
+	get_a(ld, sign, num, precision);
 	return (ld);
 }
 
-static char				*lftoa(long double num, int precision)
+static char				*ftoa(long double num, int precision)
 {
 	char				*a;
 	t_ld				*ld;
 
 	a = NULL;
-	ld = parse_ld(num, precision);
-	if (precision <= 0)
+	if (precision == NOT_SET)
 		precision = 6;
-	return (a);
+	ld = parse_ld(num, precision);
+	return (ld->str);
 }
 
 int						main(void)
@@ -54,8 +76,9 @@ int						main(void)
 	long double				num;
 	char					*a;
 
-	num = 123.123L;
-	a = lftoa(num, 6);
+	num = 123456789.123456789;
+	a = ftoa(num, 6);
+	printf("%s\n", a);
 	printf("%Lf\n", num);
 	return (0);
 }
